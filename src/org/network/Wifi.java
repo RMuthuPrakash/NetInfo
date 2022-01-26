2a00f8b9f13fed68225669bc5b8936aea7c51f81
RMuthuPrakash <prakashmuthu216@gmail.com>
2022-01-26 20:14:56 +0530

    i pushed the code

---
+++ src/org/network/Wifi.java
@@ -0,0 +1,19 @@
+package org.network;
+
+public class Wifi {
+	private void wifiName() {
+		System.out.println("sf1ram");
+	}
+	public static void main(String[] args) {
+		Wifi w =new Wifi();
+		w.wifiName();
+		MobileData m =new MobileData();
+		m.dataName();
+		Lan l =new Lan();
+		l.lanName();
+		Wireless w1 =new Wireless();
+		w1.modamName();
+		
+	}
+
+}
